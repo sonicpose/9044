00f3f4cb3163777467a37c35050fda176e0e8b4e
Dan Roebuck <danroebuck@mac.com>
2017-08-10 14:39:03 +0100

    First stage of commenting

--- run.py
+++ run.py
@@ -6,52 +6,53 @@ import re
 import sys
 
 def main():
-	downloadQueue = handleInput()
-	queueDownloads(downloadQueue)
-	playlistCheck()
-	time.sleep(5)
+	while True:
+		downloadQueue = handleInput()
+		queueDownloads(downloadQueue)
+		playlistCheck()
+		time.sleep(5)
 
 	return
 
-def handleInput():
+def handleInput():	#handleInput receives and sorts the information from the front end
 	downloadQueue = []
 
-	inputText = core.read('lampp/htdocs/data.txt')
+	inputText = core.read('/opt/lampp/htdocs/data.txt')#data.txt is the file where the front end leaves the data for us
 	print(inputText)
-	#core.write('data.txt', '')
-	inputArray = core.split(inputText)
+	core.write('/opt/lampp/htdocs/data.txt', '')
+	inputArray = core.split(inputText)#inputText is split into an array so that we can interact with each line individually (Different URLs are put on different lines by the frontend).
 	print(inputArray)
 
-	i = 0
-	while i < len(inputArray):
+	i = 0#i is just an integer used for counting the times round a loop
+	while i < len(inputArray):#This loop allows us to perform the same function on each line of the array. In this case it goes through each line and sorts it into a video url, a playlist url, or random text. And then the appropriate response is carried out
 		inputData = inputArray[i]
 		print(inputData)
 
-		if 'list=' in inputData:
+		if 'list=' in inputData:#'list=' is unique to playlist urls
 			core.log('playlists', inputData)
-		if 'v=' in inputData:
+		if 'v=' in inputData:#v= is unique to video urls
 			downloadQueue.append(inputData)
 
 		i = i + 1
 
 	return downloadQueue
 
-def download(videoURL):
+def download(videoURL):#Download takes a video url and downloads the video
 	cmd = 'youtube-dl -i ' + videoURL
-	txt = "cmd = " + cmd
+	txt = "cd Downloads \n cmd = " + cmd
 	log(txt)
 	os.system(cmd)
 
 	return True
 
-def playlist(playlistURL):
-	videoIDs = scrape(playlistURL)
+def playlist(playlistURL):#Does all the playlist stuff
+	videoIDs = scrape(playlistURL)#Scrapes the URL page and gets the video ids/urls
 	print(videoIDs)
-	playlistID = playlistURL.split('list=')[1]
-	unwatchedVideoIDs = watchedCheck(playlistID, videoIDs)
+	playlistID = playlistURL.split('list=')[1]#converts playlist url into id for database
+	unwatchedVideoIDs = watchedCheck(playlistID, videoIDs)#Checks what videos in playlist have already been downloaded
 
 	i = 0
-	while i < len(unwatchedVideoIDs):
+	while i < len(unwatchedVideoIDs):#Each iteration of the loop downloads a unwatched video in the playlist
 		videoURL = 'http://www.youtube.com/watch?v='+unwatchedVideoIDs[i]
 		print(videoURL)
 		if download(videoURL):
@@ -67,7 +68,7 @@ def scrape(playlistURL):
 
 	return search_results
 
-def watchedCheck(playlistID, videoIDs):
+def watchedCheck(playlistID, videoIDs):#Checks what videos in the playlist have already been downloaded (watched).
 	unwatchedVideoIDs = []
 	watchedList = core.read(playlistID)
 	watchedIDs = core.split(watchedList)
@@ -88,7 +89,7 @@ def log(text):
 
 	return True
 
-def queueDownloads(downloadQueue):
+def queueDownloads(downloadQueue):#Runs a loop for every video not in a playlist that needs to be downloaded
 	i = 0
 	while i < len(downloadQueue):
 		download(downloadQueue[i])
@@ -96,7 +97,7 @@ def queueDownloads(downloadQueue):
 
 	return True
 
-def playlistCheck():
+def playlistCheck():#Checks what playlists are being watched/should have new videos downloaded from then begins the playlist download process
 	playlists = core.split(core.read('playlists'))
 
 	i = 0
